327abb8e34a125abfcd619c2201d3051396e5e68
Nahom <Nahomdagnewnegash@gmail.com>
2020-09-14 00:28:49 -0400

    Initial Commit

---
+++ Ray/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Ray
+//
+//  Created by Nahom Negash on 9/14/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
